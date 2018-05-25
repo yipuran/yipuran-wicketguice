542559f43d5088e59e8fad5b5953a455f89129cf
yipuran <postyip2@gmail.com>
2018-05-25 21:35:01 +0900

    no message

---
+++ src/main/java/org/yipuran/wicketguice/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Wicket Google guice パッケージ（JSR-330サポート）.
+ * Ｗｉｃｋｅｔを使用するページクラス最下層インスタンスまでの、Google guuiceインジェクションを実行する。
+ * Ｗｉｃｋｅｔの WebApplication の init() 処理の中で実行することを前提にしている。
+ */
+package org.yipuran.wicketguice;
